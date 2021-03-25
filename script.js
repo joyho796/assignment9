cb8b751cecaa77a405f84080763622543c81921a
joyho796 <56969406+joyho796@users.noreply.github.com>
2021-03-26 00:46:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+//data = {"song_list":[ {"song": {"title":"Love Story", "artist(s)":["Taylor Swift"], "genre(s)":["Pop"], "year":"2008" } }, {"song": {"title":"Heal the World", "artist(s)":["Michael Jackson"], "genre(s)":["Pop"], "year":"1991" } }, {"song": {"title":"Bad Romance", "artist(s)":["Lady Gaga"], "genre(s)":["Pop"], "year":"2008" } }, {"song": {"title":"Writing's on the Wall", "artist(s)":["Sam Smith"], "genre(s)":["Pop"], "year":"2015" } }, {"song": {"title":"The Phantom of the Opera", "artist(s)":["Andrew Lloyd Webber"], "genre(s)":["Musicals"], "year":"2008" } }, {"song": {"title":"I Dreamed a Dream", "artist(s)":["Randy Graff"], "genre(s)":["Musicals"], "year":"1987" } }, {"song": {"title":"Can You Feel the Love Tonight", "artist(s)":["Elton John"], "genre(s)":["Musicals", "Movie"], "year":"1994" } }, {"song": {"title":"Into the Unknown", "artist(s)":["Idina Menzel", "Aurora"], "genre(s)":["Musicals", "Movie"], "year":"2019" } }, {"song": {"title":"Airplanes", "artist(s)":["B.o.B", "Hayley Williams"], "genre(s)":["Pop", "Rap"], "year":"2010" } }, {"song": {"title":"Love the Way You Lie", "artist(s)":["Eminem", "Rhianna"], "genre(s)":["Pop", "Rap"], "year":"2010" } }, ]};
+
+function onLoad() {
+
+    request = new XMLHttpRequest();
+
+    request.open("GET", "data.json", true);
+
+    request.onreadystatechange = function() {
+
+        if (request.readyState == 4 && request.status == 200) {
+
+            result = request.responseText;
+
+            document.getElementById("json_str").innerHTML = JSON.stringify(result);
+
+
+        } else if (request.readyState == 4 && request.status != 200) {
+
+            document.getElementById("messages").innerHTML = "Whoops, something went terribly wrong!";
+
+        } else if (request.readyState == 3) {
+
+            document.getElementById("messages").innerHTML = "Come back soon!";
+
+        }
+
+            }//end on readystate change
+
+        // Step 4: fire off the HTTP request
+            request.send();
+            console.log("4 request sent");
+
+}
